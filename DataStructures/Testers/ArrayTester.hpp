dded33b5a8cc728a659ce4e0017689a6e00b2c94
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-02-06 09:06:13 -0700

    added arraytest hpp.

--- DataStructures/Testers/ArrayTester.hpp
+++ DataStructures/Testers/ArrayTester.hpp
@@ -9,6 +9,16 @@
 #ifndef ArrayTester_hpp
 #define ArrayTester_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include "../Model/Structures/Array.hpp"
+#include "../Controller/FileController.hpp"
+#include "../Controller/Tools/Timer.hpp"
+class ArrayTester
+{
+public:
+    void testArrayUse();
+    void testAdvancedArray();
+}
 
 #endif /* ArrayTester_hpp */
+
