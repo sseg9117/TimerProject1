7067fd0826ac59e599a6789b21ad1d34e6cab285
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-02-06 09:52:18 -0700

    added a linearNode to my node prject with classes and constructors.

---
+++ DataStructures/Model/Nodes/LinearNode.hpp
@@ -0,0 +1,70 @@
+//
+//  LinearNode.hpp
+//  TimerProject
+//
+//  Created by Segota, Sheradon on 2/6/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#include "Node.hpp"
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+template <class Type>
+Class LinearNode : public Node<type>
+{
+private:
+
+protected:
+    LinearNode<Type> * next;
+public:
+    //Constuctors
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode (Type data, LInearNode<Type> * next);
+    //Methods
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+//Constructors
+
+/**
+ this constuctor is used for building the raw stucture before values are known.
+ */
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next=nullptr;
+}
+
+/**
+ This constuctor is used to create a reference to a instand with data.
+ */
+template <class Type>
+LInearNOde<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+/**
+ This constructor is used to create a reference to a instance with data and a known link.
+ */
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+//Accessor methods
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer)
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+
+#endif /* LinearNode_hpp */
