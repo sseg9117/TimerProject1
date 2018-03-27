1e0310f01464632f1fe2729a4dce8f4ab69caf4b
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-03-27 09:20:53 -0600

    added tree class.

---
+++ DataStructures/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,28 @@
+//
+//  BinaryTreeNode.hpp
+//  TimerProject
+//
+//  Created by Segota, Sheradon on 3/27/18.
+//  Copyright © 2018 Segota, Sheradon. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class BinaryNode : public Node<Type>
+{
+    
+public:
+    
+protected:
+    
+private:
+    
+};
+
+
+
+#endif /* BinaryTreeNode_hpp */
